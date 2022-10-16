5aa2e7e74756b112e826e95891c248bdd9e56828
Cleverson Junior <75210280+cleversonasj@users.noreply.github.com>
2022-10-15 22:36:15 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ $('.carousel').slick({
   dots: false,
   infinite: true,
   speed: 500,
-  slidesToShow: 1,
+  slidesToShow: 3,
   slidesToScroll: 1,
   arrows: false,
   autoplay: true,
@@ -12,7 +12,7 @@ $('.carousel').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
         dots: true
